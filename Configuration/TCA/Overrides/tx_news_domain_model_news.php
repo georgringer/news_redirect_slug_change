bf8b152feb53db6e974b1351d8faf027945f6d28
Georg Ringer <georg.ringer@gmail.com>
2023-03-28 21:19:33 +0200

    [TASK] Support new tca select format

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -5,7 +5,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() >= 12) {
     $GLOBALS['TCA']['sys_redirect']['columns']['creation_type']['config']['items'][] = [
-        'news',
-        6322,
+        'label' => 'news',
+        'value' => 6322,
     ];
 }
